0319c408eb9f244bd1ecdd7561ef2b5f5bf0090e
Rocky Szeto <86951902+ggx-rockyszeto@users.noreply.github.com>
2023-05-29 12:25:18 +0800

    Added Installation Script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#Install Homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Install Chrome from Homebrew Repository
+brew install --cask google-chrome
+
+#Install Slack from Homebrew Repository
+brew install --cask slack
+
+#Install Adobe Acrobat Reader from Homebrew Repository
+brew install --cask adobe-acrobat-reader
+
+#Install Zoom Client from Homebrew Repository
+brew install --cask zoom
+
